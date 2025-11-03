b4e99b13c5c47b24704d9973c135ecef4c4020be
Mohitbamania <148422851+Mohitbamania@users.noreply.github.com>
2025-11-03 21:52:36 +0530

    Add Migration file for student table

---
+++ database/migrations/2025_11_03_161228_create_students_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('enrollement_no')->unique();
+            $table->enum('gender', ['Male', 'Female', 'Other']);
+            $table->string('mobile_number', 10)->unique();
+            $table->string('email_id')->unique();
+            $table->string('password');
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('students');
+    }
+};
